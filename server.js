538c4e0b82431508e654eeca723940e2d331357c
samar <samarlarbi2004@gmail.com>
2024-11-22 23:08:38 +0100

    .*.*

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json())
  
 
 app.get('/api', (req, res) => {
-    console.log("req comming")
+    console.log("hii")
   res.status(200).json({ message: 'hello from express' });
 });
 
