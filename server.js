1759a98d243c69b9121da320b826009d28b393a3
samar <samarlarbi2004@gmail.com>
2024-11-22 22:40:35 +0100

    .

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 import express from 'express';
-import cors from 'cors'
-const app =express()
-app.use(cors())
-const port = 3000
+import cors from 'cors';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+dotenv.config()
+const port = 3001;
 
+const app = express();
+app.use(cors());
+app.use(express.json())
+ 
 
-app.get("/",(req,res)=>{
-
-    res.status(200).json({message:"hello from express"})
-
-})
-app.listen(port,()=>{
-    console.log(`server running on port ${port}`)
-
+app.get('/api', (req, res) => {
+    console.log("req comming")
+  res.status(200).json({ message: 'hello from express' });
 });
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+// mongoose.connect(process.env.ATLAS_URI).then(()=>{
+//     console.log("connected succ")
+//     }).catch((error)=>{
+//       console.log("--------------------------------------------------")
+//       console.log(error)
+//     })
+
