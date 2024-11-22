f3c72781040dbbfa7466c8aaab7a42727f5c4809
samar <samarlarbi2004@gmail.com>
2024-11-22 20:54:14 +0100

    Set up Express.js server and added nodemon for development

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ import cors from 'cors'
 const app =express()
 app.use(cors())
 const port = 3000
+
+
+app.get("/",(req,res)=>{
+
+    res.status(200).json({message:"hello from express"})
+
+})
 app.listen(port,()=>{
     console.log(`server running on port ${port}`)
 
